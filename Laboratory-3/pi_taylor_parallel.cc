53feed8e6eaec0c56915ec4912e0da1dba0efa8a
DiegoBielsa <776564@unizar.es>
2023-10-19 18:52:20 +0200

    Lab3 3: lab session

--- Laboratory-3/pi_taylor_parallel.cc
+++ Laboratory-3/pi_taylor_parallel.cc
@@ -13,8 +13,11 @@ void
 pi_taylor_chunk(std::vector<my_float> &output,
         size_t thread_id, size_t start_step, size_t stop_step) {
 
-
-
+    int sign = start_step & 0x1 ? -1 : 1;
+    for (size_t n = start_step; n < stop_step; n++) {
+        output[n] = sign / static_cast<float>(2 * n + 1);
+        sign = -sign;
+    }
 }
 
 std::pair<size_t, size_t>
@@ -43,10 +46,28 @@ int main(int argc, const char *argv[]) {
     auto steps = ret_pair.first;
     auto threads = ret_pair.second;
 
-    my_float pi;
+    int num_divisions = steps / threads;
 
-    // please complete missing parts
+    std::vector<my_float> output(steps);
+    std::thread thread_pool[threads];
 
+    for (int i = 0; i < threads; i++) {
+        int start = i * num_divisions;
+        int stop = i == threads - 1 ? steps - 1 : ((i + 1) * num_divisions) - 1;
+        thread_pool[i] = std::thread(pi_taylor_chunk, ref(output), i, start, stop);
+    }
+    
+    my_float pi = 0.0f;
+
+    for (size_t i = 0; i < threads; ++i) {
+        thread_pool[i].join();
+    }
+
+    for (int i = 0; i < steps; i++) {
+        pi += output[i];
+    }
+
+    pi *= 4;
 
     std::cout << "For " << steps << ", pi value: "
         << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
--- Laboratory-3/pi_taylor_sequential.cc
+++ Laboratory-3/pi_taylor_sequential.cc
@@ -1,12 +1,21 @@
 #include <iomanip>
 #include <iostream>
 #include <limits>
+#include <chrono>
 
 // Allow to change the floating point type
 using my_float = long double;
 
 my_float pi_taylor(size_t steps) {
-    return 0.0f;
+    int sign = 1;
+    float sum = 0;
+
+    for (size_t n = 0; n < steps; n++) {
+        sum += sign / static_cast<float>(2 * n + 1);
+        sign = -sign;
+    }
+
+    return 4.0f * sum;
 }
 
 int main(int argc, const char *argv[]) {
@@ -18,10 +27,19 @@ int main(int argc, const char *argv[]) {
 
     }
 
+    // Using time point and system_clock
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+
     size_t steps = std::stoll(argv[1]);
     auto pi = pi_taylor(steps);
+    
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
 
     std::cout << "For " << steps << ", pi value: "
         << std::setprecision(std::numeric_limits<my_float>::digits10 + 1)
         << pi << std::endl;
+
+    std::cout << "time in seconds: " << elapsed_seconds.count() << "s" << std::endl;
 }
