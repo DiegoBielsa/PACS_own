2e6031ad2e0bf746cb71f8dbf22be69d7ffc48d7
DiegoBielsa <776564@unizar.es>
2023-11-02 18:49:22 +0100

    Lab3: lab session

--- Laboratory-4/include/thread_pool.hpp
+++ Laboratory-4/include/thread_pool.hpp
@@ -9,20 +9,33 @@
 
 class thread_pool
 {
+  std::atomic<bool> _done;
+  size_t _thread_count;
+  threadsafe_queue<std::function<void()>> _work_queue;
+  std::vector<std::thread> _threads;
   join_threads _joiner;
 
   using task_type = void();
 
   void worker_thread()
   {
-
+    while(!_done) {
+      std::function<task_type> task;
+      if (_work_queue.try_pop(task)) {
+        task();
+      } else {
+        std::this_thread::yield();
+      }
+    }
   }
 
   public:
   thread_pool(size_t num_threads = std::thread::hardware_concurrency())
-    : // please complete
+    : _done(false), _thread_count(num_threads), _joiner(_threads)
   {
-      // complete
+      for (size_t i = 0; i < _thread_count; ++i) {
+        _threads.push_back(std::thread(&thread_pool::worker_thread, this));
+      }
 
   }
 
@@ -34,13 +47,13 @@ class thread_pool
   void wait()
   {
       // wait for completion
-
+    
       // active waiting
   }
 
   template<typename F>
     void submit(F f)
     {
-	// please complete
+      _work_queue.push(std::function<task_type>(f));
     }
 };
--- Laboratory-4/include/threadsafe_queue.hpp
+++ Laboratory-4/include/threadsafe_queue.hpp
@@ -9,40 +9,58 @@ template<typename T>
 class threadsafe_queue
 {
   private:
-      // please complete
+      std::mutex mut;
+      std::queue<T> data_queue;
+      std::condition_variable data_cond;
 
   public:
     threadsafe_queue() {}
 
     threadsafe_queue(const threadsafe_queue& other)
     {
-	// please complete
+        std::lock_guard<std::mutex> lk(other.mut);
+	    data_queue = other.data_queue;
     }
 
     threadsafe_queue& operator=(const threadsafe_queue&) = delete;
 
     void push(T new_value)
     {
-	// please complete
+        
+        std::lock_guard<std::mutex> lk(this->mut);
+        data_queue.push(new_value);
+        data_cond.notify_one();
     }
 
     bool try_pop(T& value)
     {
-	// please complete
+	    if (this->empty()) return false;
+        value = data_queue.front();
+        return true;
     }
 
     void wait_and_pop(T& value)
     {
-	// please complete
+	    std::unique_lock<std::mutex> lk(this->mut);
+        data_cond.wait(lk, try_pop(value));
+        data_queue.pop();
+        lk.unlock();
     }
 
     std::shared_ptr<T> wait_and_pop()
     {
-	// please complete
+	    std::unique_lock<std::mutex> lk(this->mut);
+        T value;
+        data_cond.wait(lk, try_pop(value));
+        std::shared_ptr<T> value_ptr = std::make_shared<T>(value);
+        data_queue.pop();
+        lk.unlock();
+        return value_ptr;
     }
 
     bool empty() const
     {
-	// please complete
+        std::lock_guard<std::mutex> lk(this->mut);
+	    return data_queue.empty();
     }
 };
